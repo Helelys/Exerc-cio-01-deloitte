6b2f4e922932c14fdb01dc5a55cbfb34caec6e35
bootcamp01 <bootcamp01@deloitte.com>
2025-05-06 14:01:59 -0300

    adicionando quebra de linhas

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
         pessoa2.autenticadorDeIdade();
 
         if (pessoa1.getIdade() > pessoa2.getIdade()) {
-            System.out.printf("%s é mais velho, pois tem %d anos enquanto %s tem apenas %d", pessoa1.getNome(), pessoa1.getIdade(), pessoa2.getNome(), pessoa2.getIdade());
+            System.out.printf("%s é mais velho, pois tem %d anos enquanto %s tem apenas %d.\n", pessoa1.getNome(), pessoa1.getIdade(), pessoa2.getNome(), pessoa2.getIdade());
         } else if (pessoa1.getIdade() == pessoa2.getIdade()){
-            System.out.printf("%s e %s tem a mesma idade, pois ambos tem %d anos.", pessoa1.getNome(), pessoa2.getNome(), pessoa1.getIdade());
+            System.out.printf("%s e %s tem a mesma idade, pois ambos tem %d anos.\n", pessoa1.getNome(), pessoa2.getNome(), pessoa1.getIdade());
         } else {
-            System.out.printf("%s é mais velho, pois tem %d anos enquanto %s tem apenas %d", pessoa2.getNome(), pessoa2.getIdade(), pessoa1.getNome(), pessoa1.getIdade());
+            System.out.printf("%s é mais velho, pois tem %d anos enquanto %s tem apenas %d.\n", pessoa2.getNome(), pessoa2.getIdade(), pessoa1.getNome(), pessoa1.getIdade());
         }
     }
 }
